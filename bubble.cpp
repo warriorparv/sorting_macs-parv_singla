7297163b1aae1b64d3617d5d45931e6d7381cfda
warriorparv <parvsinglap@gmail.com>
2026-03-23 23:06:24 +0530

    Create bubble.cpp

---
+++ bubble.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    int iterations = 0;
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            iterations++;
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+    return iterations;
+}
