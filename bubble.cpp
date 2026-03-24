1de547b6f1c2771c159b2f63b5ff2900164a6b0c
warriorparv <parvsinglap@gmail.com>
2026-03-24 21:22:35 +0530

    Update bubble.cpp

--- bubble.cpp
+++ bubble.cpp
@@ -1,15 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-int bubbleSort(vector<int>& arr) {
-    int n = arr.size();
-    int iterations = 0;
-    for(int i = 0; i < n-1; i++) {
-        for(int j = 0; j < n-i-1; j++) {
-            iterations++;
-            if(arr[j] > arr[j+1]) {
-                swap(arr[j], arr[j+1]);
+
+int bubble_sort(vector<int> &v) {
+    int n = v.size();
+    int count = 0;
+
+    for(int i = n - 1; i > 0; i--) {
+        for(int j = 0; j < i; j++) {
+            count++;
+            if(v[j] > v[j + 1]) {
+                int temp = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = temp;
             }
         }
     }
-    return iterations;
+    return count;
 }
